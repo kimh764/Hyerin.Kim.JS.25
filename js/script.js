6c959682f7f7cbf8713e17c93e42a0b24d352dd8
Hyerin Kim <kimh764@newschool.edu>
2024-02-04 01:41:11 -0500

    fixed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-/*
-PIE CHART
-================================================ */
-var pieData = {
-  labels: ["Soulmate", "Safery", "Leran", "Culture"],
-  series: [9, 7, 6, 5],
-};
-
-var pieOptions = {
-  width: "100%",
-  height: "440px",
-};
-
-new Chartist.Pie(".pie-chart", pieData, pieOptions);
-/*
-BAR CHART
-================================================ */
-var barData = {
-  labels: ["2020s", "2021s", "2024s"],
-  series: [[10, 16, 29]],
-};
 
 var barOptions = {
   axisY: {
